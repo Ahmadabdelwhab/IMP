4678a764832efb92886aef5834a69e68858d6328
Ahmadabdelwhab <ahmad.sayed.abdelwhab@gmail.com>
2023-12-15 01:31:04 +0200

    99

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,16 @@ import base64
 from io import BytesIO
 import numpy as np
 from PIL import Image
-from flask import Flask, request, jsonify , render_template , send_file
+from flask import Flask, request, jsonify, render_template, send_file
 import cv2
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
 
 def base64_to_img(img_64):
     '''convert image from numpy array to image png format'''
-    #convert from base64 to image
+    # convert from base64 to image
     image_data = base64.b64decode(img_64)
     # Convert the decoded data to a NumPy array
     image_array = np.frombuffer(image_data, dtype=np.uint8)
@@ -18,12 +19,16 @@ def base64_to_img(img_64):
     image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
     return image
 
+
 def process_for_upload(edited_img):
     '''convert images to bytes to send later'''
     _, buffer = cv2.imencode('.png', edited_img)
     # Convert the image buffer to bytes
     image_bytes = buffer.tobytes()
     return image_bytes
+# filters to apply
+
+
 def canny(img):
     blurred = cv2.GaussianBlur(img, (5, 5), 0)
     # Apply Canny edge detection
@@ -40,6 +45,26 @@ def sobel(img):
     # Convert the magnitude values to uint8
     magnitude_uint8 = np.uint8(magnitude)
     return magnitude_uint8
+
+
+def perwitt(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    perwitt_kernel_x = np.array([[-1, 0, 1],
+                                [-1, 0, 1],
+                                [-1, 0, 1]])
+    edges_x = cv2.filter2D(img, -1, perwitt_kernel_x)
+# Apply Perwitt edge detection for vertical edges
+    perwitt_kernel_y = np.array([[-1, -1, -1],
+                                [0, 0, 0],
+                                [1, 1, 1]])
+    edges_y = cv2.filter2D(img, -1, perwitt_kernel_y)
+    # Combine the horizontal and vertical edges
+    edges_x = edges_x.astype(np.float64)
+    edges_y = edges_y.astype(np.float64)
+    edges = cv2.magnitude(edges_x, edges_y)
+    return edges
+
+
 def segment(img):
     image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # Flatten the image into a 2D array of pixels
@@ -49,7 +74,8 @@ def segment(img):
     # Define criteria and apply kmeans()
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
     k = 3  # Number of clusters
-    _, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    _, labels, centers = cv2.kmeans(
+        pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
     # Convert back to 8-bit values
     centers = np.uint8(centers)
     # Map the labels to the centers
@@ -58,24 +84,38 @@ def segment(img):
     segmented_image = segmented_image.reshape(image_rgb.shape)
     return segmented_image
 
+
 def detect_objects(img):
     # Load the pre-trained Haarcascades classifier for face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     # Convert the image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect faces in the image
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.3, minNeighbors=5)
     # Draw rectangles around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     return img
-filters = {
-    "canny":canny,
-    "sobel":sobel, 
-    "segment":segment,
-    "detection":detect_objects
-}
 
+def histogram_equalization(img):
+    # Convert the image to grayscale if it's not already
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Apply histogram equalization
+    equalized_img = cv2.equalizeHist(img)
+
+    return equalized_img
+filters = {
+    "canny": canny,
+    "sobel": sobel,
+    "segment": segment,
+    "detection": detect_objects, 
+    "perwitt":perwitt,
+    "hist":histogram_equalization
+}
 
 
 @app.route("/")
@@ -83,40 +123,35 @@ def home():
     '''renders template'''
     return render_template("index.html")
 
-
-
-
-
-
-
-
+ 
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    '''apply filters and return image'''
     try:
         data = request.get_json()
-        if 'image' in data: 
+        if 'image' in data and 'filter' in data:
             img_64 = data['image']
             img = base64_to_img(img_64)
-            filter = filters[data["filter"]]
-            edited_img = filter(img)
-            img_bytes = process_for_upload(edited_img)
-            # Send the image in the response
-            return send_file(BytesIO(img_bytes), mimetype='image/png')
+            filter_to_apply = filters.get(data["filter"])
             
-        return jsonify({'error': 'No image data provided.'}), 400
+            if filter_to_apply:
+                edited_img, hist = filter_to_apply(img)
+                img_bytes = process_for_upload(edited_img)
+                img_bytes = BytesIO(img_bytes)
+                 
+                # Encode the images and histogram as base64 strings
+                original_image_base64 = base64.b64encode(img_64.encode('utf-8')).decode('utf-8')
+                processed_image_base64 = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
+                # Return the result as a JSON response
+                return jsonify({
+                    "processed_image": processed_image_base64,
+                })
+            else:
+                return jsonify({'error': 'Invalid filter name.'}), 400
+        else:
+            return jsonify({'error': 'Invalid request format. Please provide "image" and "filter" in the request.'}), 400
     except Exception as e:
         print(str(e))
         return jsonify({'error': f'Error processing the image: {str(e)}'}), 500
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
