0243a49808ee7ce14a46048aa0743c5e43aa34cd
Ahmadabdelwhab <ahmad.sayed.abdelwhab@gmail.com>
2023-12-14 12:38:43 +0200

    canny , sobel , segmen , detection

--- app/__init__.py
+++ app/__init__.py
@@ -29,9 +29,55 @@ def canny(img):
     # Apply Canny edge detection
     edges = cv2.Canny(blurred, 50, 150)
     return edges
+
+
+def sobel(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    # Compute the magnitude of the gradient
+    magnitude = np.sqrt(sobel_x**2 + sobel_y**2)
+    # Convert the magnitude values to uint8
+    magnitude_uint8 = np.uint8(magnitude)
+    return magnitude_uint8
+def segment(img):
+    image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Flatten the image into a 2D array of pixels
+    pixels = image_rgb.reshape((-1, 3))
+    # Convert to float32
+    pixels = np.float32(pixels)
+    # Define criteria and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    k = 3  # Number of clusters
+    _, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    # Convert back to 8-bit values
+    centers = np.uint8(centers)
+    # Map the labels to the centers
+    segmented_image = centers[labels.flatten()]
+    # Reshape back to the original image dimensions
+    segmented_image = segmented_image.reshape(image_rgb.shape)
+    return segmented_image
+
+def detect_objects(img):
+    # Load the pre-trained Haarcascades classifier for face detection
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    # Draw rectangles around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return img
 filters = {
-    "canny":canny
+    "canny":canny,
+    "sobel":sobel, 
+    "segment":segment,
+    "detection":detect_objects
 }
+
+
+
 @app.route("/")
 def home():
     '''renders template'''
